4c293e7c69466e623b495477b4ebbbb99fcaae0d
Sulaiman Marey <adammarey51@gmail.com>
2022-08-31 23:27:04 -0700

    complete the mainContext component with all tasks hooks

---
+++ MainProvider.js
@@ -0,0 +1,47 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { useTaskReducer } from './ListReducer';
+
+const MainContext = React.createContext();
+
+const useMain = () => useContext(MainContext);
+
+function MainProvider({ children }) {
+  const [isFormVisible, setFormVisible] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [isEdit, setEdit] = useState(false);
+  const [currentTaskId, setCurrentTaskId] = useState('');
+  const [isDarkMode, setDarkMode] = useState(false);
+  const taskList = JSON.parse(localStorage.getItem('Tasks'));
+  const [countDown, dispatch] = useTaskReducer();
+
+  useEffect(() => {
+    taskList && setTasks(taskList);
+  }, []);
+
+  useEffect(() => {
+    tasks && localStorage.setItem('Tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  return (
+    <MainContext.Provider
+      value={{
+        isFormVisible,
+        tasks,
+        currentTaskId,
+        isEdit,
+        isDarkMode,
+        countDown,
+        setFormVisible,
+        setTasks,
+        setCurrentTaskId,
+        setEdit,
+        setDarkMode,
+        dispatch,
+      }}
+    >
+      {children}
+    </MainContext.Provider>
+  );
+}
+
+export { MainProvider, useMain };
