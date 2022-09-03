6ee401fa000e331ac585f9829219dba961f8e221
Sulaiman Marey <adammarey51@gmail.com>
2022-09-03 10:56:47 -0700

    added all constantes

---
+++ utility.js
@@ -0,0 +1,12 @@
+// constant
+export const DATE_FORMAT = 'en-in';
+export const START_STATUS = 'Start';
+export const RESTART_STATUS = 'Restart';
+export const PAUSE_STATUS = 'Pause';
+export const DOCUMENT_TITLE = 'Pomodoro';
+
+export const CURRENT_MONTH_FILTER = 'Current Month';
+export const CURRENT_WEEK_FILTER = 'Current Week';
+export const TODAY_FILTER = 'Today';
+export const PENDING_TASK_FILTER = 'Pending Task';
+export const COMPLETED_TASK_FILTER = 'Completed Task';
